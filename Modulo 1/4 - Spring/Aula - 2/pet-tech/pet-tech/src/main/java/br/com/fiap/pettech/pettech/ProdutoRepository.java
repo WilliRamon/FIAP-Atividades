d5a09778d8b1ac8242055398034b3120a61aaa5a
WilliRamon <acdc.ramon@hotmail.com>
2024-05-05 03:43:08 -0300

    Conexões com banco e classe de entidade

---
+++ Modulo 1/4 - Spring/Aula - 2/pet-tech/pet-tech/src/main/java/br/com/fiap/pettech/pettech/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.pettech.pettech;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+}
