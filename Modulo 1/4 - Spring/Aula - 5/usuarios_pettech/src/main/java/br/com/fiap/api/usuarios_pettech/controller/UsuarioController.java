28be146e7423830f419fe0dbebfb1bb775ae5f96
WilliRamon <acdc.ramon@hotmail.com>
2024-05-26 03:36:04 -0300

    Implementando Controller - API Usuario

---
+++ Modulo 1/4 - Spring/Aula - 5/usuarios_pettech/src/main/java/br/com/fiap/api/usuarios_pettech/controller/UsuarioController.java
@@ -0,0 +1,55 @@
+package br.com.fiap.api.usuarios_pettech.controller;
+
+import br.com.fiap.api.usuarios_pettech.dto.UsuarioDTO;
+import br.com.fiap.api.usuarios_pettech.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @Autowired
+    public UsuarioController(UsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UsuarioDTO>> findAll(@PageableDefault(size = 10, page = 0, sort = "nome")Pageable pageable){
+        Page<UsuarioDTO> usuariosDTOS = usuarioService.findAll(pageable);
+        return ResponseEntity.ok(usuariosDTOS);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> findById(@PathVariable Long id){
+        UsuarioDTO usuarioDTO = usuarioService.findById(id);
+        return ResponseEntity.ok(usuarioDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> save(@RequestBody UsuarioDTO usuarioDTO){
+        UsuarioDTO savedUsuario = usuarioService.save(usuarioDTO);
+        return new ResponseEntity<>(savedUsuario, HttpStatus.CREATED);
+        //return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(usuarioDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> update(@PathVariable Long id, @RequestBody UsuarioDTO usuarioDTO){
+        UsuarioDTO updatedUsuario = usuarioService.update(id, usuarioDTO);
+        return ResponseEntity.ok(updatedUsuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        usuarioService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
