27438755f37f7b654dedd5669533bae01b2180f7
WilliRamon <acdc.ramon@hotmail.com>
2024-07-22 02:25:51 -0300

    Classe Artigo - Projeto SpringBlog

---
+++ Modulo 2/3 - Spring Data com NoSQL/springblog/src/main/java/com/fiap/springblog/model/Artigo.java
@@ -0,0 +1,20 @@
+package com.fiap.springblog.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalTime;
+
+@Document
+@Data
+public class Artigo {
+
+    @Id
+    private String codigo;
+    private String titulo;
+    private LocalTime data;
+    private String texto;
+    private String url;
+    private Integer status;
+}
