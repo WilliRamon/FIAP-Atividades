680900de189bec1d9701a557967d30dce1f24789
WilliRamon <acdc.ramon@hotmail.com>
2024-08-05 03:07:18 -0300

    EAutor

--- Modulo 2/3 - Spring Data com NoSQL/springblog/src/main/java/com/fiap/springblog/service/impl/ArtigoServiceImpl.java
+++ Modulo 2/3 - Spring Data com NoSQL/springblog/src/main/java/com/fiap/springblog/service/impl/ArtigoServiceImpl.java
@@ -1,7 +1,9 @@
 package com.fiap.springblog.service.impl;
 
 import com.fiap.springblog.model.Artigo;
+import com.fiap.springblog.model.Autor;
 import com.fiap.springblog.repository.ArtigoRepository;
+import com.fiap.springblog.repository.AutorRepository;
 import com.fiap.springblog.service.ArtigoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,8 @@ public class ArtigoServiceImpl implements ArtigoService {
 
     @Autowired
     private ArtigoRepository artigoRepository;
+    @Autowired
+    private AutorRepository autorRepository;
     @Override
     public List<Artigo> obterTodos() {
         return this.artigoRepository.findAll();
@@ -25,7 +29,23 @@ public class ArtigoServiceImpl implements ArtigoService {
                 .orElseThrow(() -> new IllegalArgumentException("Artigo não existe"));
     }
     @Override
-    public Artigo criar(Artigo artigo) {
+    public Artigo criar(Artigo artigo) {s
+
+        // Se o autor existe
+        if(artigo.getAutor().getCodigo() != null){
+
+            // Recuperar o autor
+            Autor autor = this.autorRepository
+                    .findById(artigo.getAutor().getCodigo())
+                    .orElseThrow(() -> new IllegalArgumentException("Autor inexistente."));
+
+            //Define o autor no artigo
+            artigo.setAutor(autor);
+
+        }else{
+            //Senão, não atribuir um autor ao código
+            artigo.setAutor(null);
+        }
         return this.artigoRepository.save(artigo);
     }
 }
